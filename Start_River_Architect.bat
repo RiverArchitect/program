5b06263f9565184ab474ad180d5dd90295669a0a
Kenneth Gregory Larrieu <kglarrieu@ucdavis.edu>
2019-07-25 13:02:49 -0700

    made start batch file more robust

--- Start_River_Architect.bat
+++ Start_River_Architect.bat
@@ -1,6 +1,29 @@
 @echo RIVER ARCHITECT
 @echo Loading (please wait) ...
+@echo off
 
-call "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\Scripts\propy" "%cd%\master_gui.py"
+SET _propy=""
+
+IF EXIST "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\Scripts\" (
+	SET _propy="%PROGRAMFILES%\ArcGIS\Pro\bin\Python\Scripts\propy"
+)
+
+IF EXIST "%LOCALAPPDATA%\Programs\ArcGIS\Pro\bin\Python\Scripts\" (
+	SET _propy="%LOCALAPPDATA%\Programs\ArcGIS\Pro\bin\Python\Scripts\propy"
+)
+
+IF %_propy%=="" (
+	goto err_msg
+)
+
+@echo on
+call %_propy% "%cd%\master_gui.py"
 
 exit
+
+:err_msg
+	@echo off
+	@echo: 
+	@echo ERROR: Cannot find ArcPro's Python installation. Make sure ArcPro is installed on your computer.
+	pause
+	exit
